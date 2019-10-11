58ebfbeab78418d1151f7a8dd2e76d30fd727435
Katrina Roaix <kroaix@gmail.com>
2019-10-11 11:55:06 -0400

    Set up UpdateMovie state

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const initialState = {
+  title: '',
+  director: '',
+  metascore: '',
+  stars: []
+}
 
 const UpdateMovie = props => {
+  const [state, setState] = useState(initialState);
+  
   return(
     <div>
     </div>
